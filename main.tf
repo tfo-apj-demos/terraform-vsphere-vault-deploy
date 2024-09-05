d6e50bbbd67d5d3b5a3234a3156afacb8df7a491
Aaron Evans <aarone@outlook.com.au>
2024-09-05 20:24:08 +1000

    feat: Add boundary_target module for managing Boundary targets

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ module "vault_blue" {
 }
 
 # --- Create Boundary targets for the Vault nodes
-/*module "boundary_target" {
+module "boundary_target" {
   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
   version = "~> 0.0"
 
@@ -112,7 +112,7 @@ module "vault_blue" {
   hostname_prefix = "vault_blue"
   injected_credential_library_ids = ["clvsclt_bDETPnhh75"]
   vault_address                   = "https://production.vault.11eb56d6-0f95-3a99-a33c-0242ac110007.aws.hashicorp.cloud:8200/"
-}*/
+}
 
 # --- Add Vault nodes and LB to DNS
 module "domain-name-system-management" {
