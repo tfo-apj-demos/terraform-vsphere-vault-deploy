a9ace931de1e250b1809fd9fd6a697d2b95d0da9
Aaron Evans <aarone@outlook.com.au>
2024-09-06 15:45:47 +1000

    chore: Update outputs.tf to include additional outputs for existing SSH credential library IDs and service keys

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "vault_address" {
   value = nsxt_policy_ip_address_allocation.load_balancer.allocation_ip
 }
+
+output "existing_ssh_credential_library_ids_output" {
+  value = module.boundary_target.existing_ssh_credential_library_ids
+}
+
+output "service_keys_output" {
+  value = module.boundary_target.service_keys
+}
