c15b2d5a863799536b848f8d3c97159bec4f1ac9
Aaron Evans <aarone@outlook.com.au>
2024-09-09 11:51:52 +1000

    chore: Update outputs.tf to troubleshoot existing SSH credential library IDs output

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,8 @@ output "vault_address" {
   value = nsxt_policy_ip_address_allocation.load_balancer.allocation_ip
 }
 
-output "existing_ssh_credential_library_ids_output" {
+# Troubleshooting
+/*output "existing_ssh_credential_library_ids_output" {
   value = module.boundary_target.existing_ssh_credential_library_ids
 }
 
@@ -16,4 +17,4 @@ output "host_catalog_id" {
 
 output "host_ids" {
   value = module.boundary_target.host_ids
-}
+}*/
